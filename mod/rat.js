80e0f189e4957ef66f0ca74ee01aa51371726aca
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-08-09 18:21:22 +0900

    Update rat.js

--- mod/rat.js
+++ mod/rat.js
@@ -1,4 +1,22 @@
 /*
 20230809
 rat( a, b, c, ... ) => do something
+
+function foo(x, ...args) {
+  console.log(x, args, ...args, arguments);
+}
+
+foo('a', 'b', 'c', z='d')
+
+=>
+
+a
+Array(3) [ "b", "c", "d" ]
+b c d
+Arguments
+​    0: "a"
+    ​1: "b"
+    ​2: "c"
+    ​3: "d"
+    ​length: 4
 */
